d28c39b26207569c8f2aa1e169a9164a3251ae96
Spider-has <romanh998@gmail.com>
2025-12-01 17:52:26 +0300

    добавили все сигнатуры методов и delete

--- 1_semester/practice_1_12_25/main.cpp
+++ 1_semester/practice_1_12_25/main.cpp
@@ -2,38 +2,106 @@
 
 namespace top
 {
-  struct p_t
+  namespace pod
   {
-    int x, y;
-  };
+    struct p_t
+    {
+      int x, y;
+    };
 
-  bool operator==(p_t a, p_t b)
+    bool operator==(p_t a, p_t b);
+    bool operator!=(p_t a, p_t b);
+
+    struct frame_t
+    {
+      p_t left_bot;
+      p_t right_top;
+    };
+  }
+  using namespace pod;
+
+  namespace figures
   {
-    return a.x == b.x && a.y == b.y;
+    struct IDraw
+    {
+      virtual p_t begin() const = 0;
+      virtual p_t next(p_t) const = 0;
+
+      virtual ~IDraw() = default;
+    };
+
+    struct Dot : IDraw
+    {
+      Dot(int x, int y);
+      Dot(p_t pt);
+
+      p_t begin() const override;
+      p_t next(p_t) const override;
+      p_t o;
+    };
+    size_t count(IDraw &d);
+
+  }
+  using namespace figures;
+
+  void makeFigures(IDraw **f, size_t size);
+  void getPoints(IDraw *figure, p_t **ps, size_t &size);
+
+  frame_t buildFrame(const p_t *ps, size_t s);
+  char *buildCanvas(frame_t f);
+  void paintCanvas(char *cnv, frame_t frame, const p_t *ps, size_t k, char f);
+  void printCanvas(const char *cnv, frame_t fr);
+}
+
+int main()
+{
+  using namespace top;
+  IDraw *f[3] = {};
+  p_t *p = nullptr;
+  size_t s = 0;
+
+  char *cnv = nullptr;
+
+  int err = 0;
+  try
+  {
+    makeFigures(f, 3);
+    for (size_t i = 0; i < 3; i++)
+    {
+      getPoints(f[i], &p, s);
+    }
+    frame_t frame = buildFrame(p, s);
+    cnv = buildCanvas(frame);
+    paintCanvas(cnv, frame, p, s, '#');
+    printCanvas(cnv, frame);
+  }
+  catch (...)
+  {
+    err = 1;
   }
 
-  bool operator!=(p_t a, p_t b)
+  for (size_t i = 0; i < 3; i++)
   {
-    return !(a == b);
+    delete[] f[i];
   }
+  delete[] f;
+  delete[] p;
+  delete[] cnv;
+  return err;
+}
 
-  struct IDraw
-  {
-    virtual p_t begin() const = 0;
-    virtual p_t next(p_t) const = 0;
-  };
+bool top::pod::operator==(p_t a, p_t b)
+{
+  return a.x == b.x && a.y == b.y;
+}
 
-  struct Dot : IDraw
-  {
-    Dot(int x, int y);
-    Dot(p_t pt);
+bool top::pod::operator!=(p_t a, p_t b)
+{
+  return !(a == b);
+}
 
-    p_t begin() const override;
-    p_t next(p_t) const override;
-    p_t o;
-  };
-
-  size_t count(IDraw &d)
+size_t top::figures::count(IDraw &d)
+{
   {
     size_t k = 0;
     p_t p = d.begin();
@@ -44,11 +112,6 @@ namespace top
     }
     return k;
   }
-}
-
-int main()
-{
-  using namespace top;
 }
 
 top::Dot::Dot(int x, int y):
@@ -65,4 +128,11 @@ top::p_t top::Dot::begin() const
 top::p_t top::Dot::next(p_t prev) const
 {
   return o;
+}
+
+void top::makeFigures(IDraw **f, size_t size)
+{
+  f[0] = new Dot(0, 0);
+  f[1] = new Dot(5, 3);
+  f[2] = new Dot(4, 4);
 }
